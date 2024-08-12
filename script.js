83f8aeb0b2a109b9c3aac20328802144a7a885ab
Er. Akash <er.akashm24@gmail.com>
2024-08-13 01:56:18 +0530

    responsive nav bar toggle done

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById('navbar-toggle').addEventListener('click', function () {
-    var links = document.getElementById('navbar-links');
-    if (links.classList.contains('expanded')) {
-        links.classList.remove('expanded');
+    var navContainer = document.getElementById('nav-bar-container');
+    if (navContainer.classList.contains('expanded')) {
+        navContainer.classList.remove('expanded');
     } else {
-        links.classList.add('expanded');
+        navContainer.classList.add('expanded');
     }
 });
